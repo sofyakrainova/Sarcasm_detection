82f71c143814bf5a6915560fcffe2214d9084f55
Sofya Krainova <sofya.krainova@gmail.com>
2024-09-18 14:37:40 +0200

    model with pre-defined emnedding

--- load_preprocess.py
+++ load_preprocess.py
@@ -1,5 +1,6 @@
 import json
 import tensorflow as tf
+import numpy as np
 
 # Load the JSON file
 with open("../sarcasm_data/all_data.json", 'r') as f:
@@ -19,7 +20,7 @@ print("Number of sentences:", len(labels))
 TRAINING_SIZE = 40000
 VOCAB_SIZE = 10000
 MAX_LENGTH = 32
-EMBEDDING_DIM = 16
+EMBEDDING_DIM = 100
 
 # Split the sentences
 train_sentences = sentences[0:TRAINING_SIZE]
@@ -36,6 +37,34 @@ vectorize_layer = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE, outpu
 vectorize_layer.adapt(train_sentences)
 
 
+# Define path to file containing the embeddings
+glove_file = "glove.6B.100d.txt"
+
+# Initialize an empty embeddings index dictionary
+glove_embeddings = {}
+
+# Read file and fill glove_embeddings with its contents
+with open(glove_file, encoding="utf8") as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype="float32")
+        glove_embeddings[word] = coefs
+
+# Create a word index dictionary
+word_index = {x:i for i,x in enumerate(vectorize_layer.get_vocabulary())}
+
+# Initialize an empty numpy array with the appropriate size
+embeddings_matrix = np.zeros((VOCAB_SIZE, EMBEDDING_DIM))
+
+# Iterate all of the words in the vocabulary and if the vector representation for
+# each word exists within GloVe's representations, save it in the embeddings_matrix array
+for word, i in word_index.items():
+    embedding_vector = glove_embeddings.get(word)
+    if embedding_vector is not None:
+        embeddings_matrix[i] = embedding_vector
+
+
 # Apply the vectorization layer on the train and test inputs
 train_sequences = vectorize_layer(train_sentences)
 test_sequences = vectorize_layer(test_sentences)
@@ -44,6 +73,8 @@ test_sequences = vectorize_layer(test_sentences)
 train_dataset_vectorized = tf.data.Dataset.from_tensor_slices((train_sequences,train_labels))
 test_dataset_vectorized = tf.data.Dataset.from_tensor_slices((test_sequences,test_labels))
 
-# Save vectorized datasets
+# Save vectorized datasets and embedding matrix
 train_dataset_vectorized.save("train_data")
 test_dataset_vectorized.save("test_data")
+with open('emb_matrix.npy', 'wb') as f:
+    np.save(f, embeddings_matrix)
--- train_model.py
+++ train_model.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 
 VOCAB_SIZE = 10000
 MAX_LENGTH = 32
-EMBEDDING_DIM = 16
+EMBEDDING_DIM = 100
 SHUFFLE_BUFFER_SIZE = 1000
 PREFETCH_BUFFER_SIZE = tf.data.AUTOTUNE
 BATCH_SIZE = 32
-EPOCHS = 20
+EPOCHS = 150
 
 print("Load datasets")
 train_data = tf.data.Dataset.load("train_data")
 test_data = tf.data.Dataset.load("test_data")
+embeddings_matrix = np.load("emb_matrix.npy")
+
 
 # Optimize the datasets for training
 train_dataset_final = (train_data
@@ -29,10 +32,15 @@ test_dataset_final = (test_data
 # Build the model
 model = tf.keras.Sequential([
     tf.keras.Input(shape=(MAX_LENGTH,)),
-    tf.keras.layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM),
+    tf.keras.layers.Embedding(
+                               input_dim=VOCAB_SIZE,
+                               output_dim=EMBEDDING_DIM,
+                               weights=[embeddings_matrix],
+                               trainable=None),
     tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
